f03431314fb12e0b5a6dce1c9c8c2040517d3087
ManateeFan <0219lyf@gmail.com>
2020-04-14 13:31:06 +0800

    Add select_test.go

---
+++ golearning/src/chp16/select/select_test.go
@@ -0,0 +1,40 @@
+package csp
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func service() string {
+	time.Sleep(time.Millisecond * 50)
+	return "Done."
+}
+
+func otherTask() {
+	fmt.Println("working on something else")
+	time.Sleep(time.Millisecond * 100)
+	fmt.Println("Task is done.")
+}
+
+func AsyncService() chan string {
+	retCh := make(chan string)
+	// retCh := make(chan string)
+	go func() {
+		ret := service()
+		fmt.Println("returned result.")
+		retCh <- ret
+		fmt.Println("service exited")
+	}()
+	return retCh
+}
+
+func TestAsyncService(t *testing.T) {
+	select {
+	case ret := <-AsyncService():
+		t.Log(ret)
+	case <-time.After(time.Millisecond * 100):
+		t.Error("time out")
+	}
+	otherTask()
+}
