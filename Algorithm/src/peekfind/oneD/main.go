d9f5497337100d84ca03099e3b276ed6f311326e
ManateeFan <0219lyf@gmail.com>
2020-09-07 09:53:44 +0800

    Add peek find one d

---
+++ Algorithm/src/peekfind/oneD/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	nums := generatrrSlice(100000000)
+	fmt.Println(nums)
+	res := peekFindDivide(nums, 0, len(nums)-1)
+	fmt.Println(res)
+}
+
+func peekFindDivide(nums []int, left int, right int) int {
+
+	mid := left + (right-left)/2
+	if (mid+1) < len(nums) && nums[mid] <= nums[mid+1] {
+		return peekFindDivide(nums, mid+1, right)
+	} else if (mid-1) >= 0 && nums[mid] <= nums[mid-1] {
+		return peekFindDivide(nums, left, mid)
+	} else {
+		return nums[mid]
+	}
+}
+
+func generatrrSlice(size int) []int {
+	slice := make([]int, size)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < size; i++ {
+		slice[i] = rand.Intn(10000)
+	}
+	return slice
+}
